72cadfd30a8678982f8d96cab0379a8cd1ab5e3b
smutahoang <anha2k47@gmail.com>
2017-08-24 16:05:51 +0200

    improve tweet feature engineering

--- src/main/java/hoang/l3s/attt/model/pseudosupervised/Classifier.java
+++ src/main/java/hoang/l3s/attt/model/pseudosupervised/Classifier.java
@@ -2,6 +2,7 @@ package hoang.l3s.attt.model.pseudosupervised;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import hoang.l3s.attt.configure.Configure;
@@ -9,7 +10,6 @@ import hoang.l3s.attt.model.Tweet;
 import hoang.l3s.attt.utils.TweetPreprocessingUtils;
 import weka.classifiers.functions.SMO;
 import weka.core.Attribute;
-import weka.core.BinarySparseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SparseInstance;
@@ -29,6 +29,9 @@ public class Classifier {
 
 	private HashMap<String, Integer> attribute2Index;
 
+	// utility variables
+	private HashSet<Integer> indiceSet;
+
 	public Classifier(List<Tweet> tweets) {
 		// TODO Auto-generated constructor stub
 	}
@@ -36,6 +39,8 @@ public class Classifier {
 	public Classifier(List<Tweet> positiveSamples, List<Tweet> negativeSamples,
 			TweetPreprocessingUtils _preprocessingUtils) {
 		System.out.println("(Re-) training a classifer");
+		// utility variables
+		indiceSet = new HashSet<Integer>();
 
 		preprocessingUtils = _preprocessingUtils;
 
@@ -57,21 +62,19 @@ public class Classifier {
 		System.out.println("=================Starting Training Model=========================");
 		svm = new SMO();
 
-		
-
 		try {
 			svm.buildClassifier(dataSet);
 			System.out.println("=================Finishing Training Model=========================");
 			// to print out attributes' weight
-			/*double[] weights = svm.sparseWeights()[0][1];
-			int[] indices = svm.sparseIndices()[0][1];
-			// weight[i] is now weight of attribute indices[i]
 
+			double[] weights = svm.sparseWeights()[0][1];
+			int[] indices = svm.sparseIndices()[0][1];
 			for (int i = 0; i < indices.length; i++) {
 				int j = indices[i];
 				System.out.printf("attribute[%d]: name = %s weight = %f\n", j, attributes.get(j).name(), weights[i]);
 			}
-*/
+
+			System.exit(-1);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -139,8 +142,9 @@ public class Classifier {
 
 		ArrayList<String> classAtt = new ArrayList<String>();
 
-		classAtt.add(Configure.RELEVANT_CLASS);
 		classAtt.add(Configure.NONRELEVANT_CLASS);
+		classAtt.add(Configure.RELEVANT_CLASS);
+		
 
 		attributes.add(new Attribute(Configure.CLASS_ATTRIBUTE, classAtt, attributeIndex));
 
@@ -155,30 +159,37 @@ public class Classifier {
 		// Tuan-Anh: use other constructor for constructing sparse instance
 		// SparseInstance(double weight, double[] attValues, int[] indices, int
 		// maxNumValues)
-		
-		double[] attValues = new double[attributes.size()];
-		Instance ins = new SparseInstance(1, attValues);
-		//Instance ins = new SparseInstance(attributes.size());
 
+		indiceSet.clear();
 		int count = 0;
 		for (String term : termsofTweet) {
 			if (attribute2Index.containsKey(term)) {
-				ins.setValue(attribute2Index.get(term), 1);
+				indiceSet.add(attribute2Index.get(term));
 			} else {
 				count++;
 			}
 		}
 
-		// [[MISSING_ATTRIBUTE]]
-		ins.setValue(attributes.size()-2, ((double) count / termsofTweet.size()));
-		
-		return ins;
+		double[] attValues = new double[indiceSet.size() + 1];
+		int[] indices = new int[indiceSet.size() + 1];
+		int i = 0;
+		for (int j : indiceSet) {
+			attValues[i] = 1;
+			indices[i] = j;
+			i++;
+		}
 
+		// [[MISSING_ATTRIBUTE]]
+		attValues[i] = ((double) count / termsofTweet.size());
+		indices[i] = attributes.size() - 2;
+
+		Instance ins = new SparseInstance(1.0, attValues, indices, attribute2Index.size());
+		return ins;
 	}
 
 	// get class of a new instance
 	public String classify(Tweet tweet) {
-		
+
 		System.out.printf("\n\n[Classification] tweet = %s\n", tweet.getText().replace('\n', ' '));
 
 		if (tweet.getTerms(preprocessingUtils).size() == 0)
@@ -189,14 +200,14 @@ public class Classifier {
 		test.setClassIndex(attributes.size() - 1);
 
 		Instance ins = getSparseInstance(tweet);
-		
+
 		test.add(ins);
-		
+
 		try {
 			int v = (int) svm.classifyInstance(test.get(0));
-			
+
 			// TODO
-			
+
 			result = test.classAttribute().value(v);
 
 		} catch (Exception e) {
